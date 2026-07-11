8fc7bbc97630d9327d445b677de9530c74764094
the khabar thread <thekhabarthread@gmail.com>
2026-07-11 19:49:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,41 @@ fetch("data/news.json")
 .then(response => response.json())
 .then(news => {
 
-    console.log(news);
+    // ===== HERO =====
 
     document.getElementById("hero-category").innerText = news[0].category;
+
     document.getElementById("hero-title").innerText = news[0].title;
+
     document.getElementById("hero-summary").innerText = news[0].summary;
+
     document.getElementById("hero-image").src = news[0].image;
+
+
+    // ===== LATEST NEWS =====
+
+    const grid = document.getElementById("news-grid");
+
+    news.forEach(item=>{
+
+        grid.innerHTML += `
+
+        <div class="card">
+
+            <img src="${item.image}" class="card-image">
+
+            <h3>${item.title}</h3>
+
+            <p>${item.summary}</p>
+
+            <a href="${item.link}" class="read-btn">
+                पूरा पढ़ें →
+            </a>
+
+        </div>
+
+        `;
+
+    });
 
 });
