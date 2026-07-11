70a5c59aa6f45915f17209a809245732a071f3dc
the khabar thread <thekhabarthread@gmail.com>
2026-07-11 19:33:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 fetch("data/news.json")
 .then(response => response.json())
-.then(data => {
+.then(news => {
 
-    console.log(data);
+    // Hero News
+    document.getElementById("hero-category").innerText = news[0].category;
+
+    document.getElementById("hero-title").innerText = news[0].title;
+
+    document.getElementById("hero-summary").innerText = news[0].summary;
+
+    document.getElementById("hero-image").src = news[0].image;
 
 });
