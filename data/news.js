14e3df543ca1e48440b5f0c5229d90060b2a6160
the khabar thread <thekhabarthread@gmail.com>
2026-07-12 12:53:02 +0530

    Update news.js

--- data/news.js
+++ data/news.js
@@ -6,19 +6,17 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const params = new URLSearchParams(window.location.search);
-
 const id = params.get("id");
 
 const container = document.getElementById("news-details");
 
+console.log("News ID:", id);
+
 async function loadNews() {
 
     if (!id) {
-
-        container.innerHTML = "<h2>News Not Found</h2>";
-
+        container.innerHTML = "<h2>No ID Found</h2>";
         return;
-
     }
 
     try {
@@ -27,59 +25,47 @@ async function loadNews() {
 
         const docSnap = await getDoc(docRef);
 
+        console.log("Exists:", docSnap.exists());
+
         if (!docSnap.exists()) {
-
             container.innerHTML = "<h2>News Not Found</h2>";
-
             return;
-
         }
 
         const news = docSnap.data();
 
-        container.innerHTML = `
+        console.log(news);
 
+        container.innerHTML = `
         <div class="single-news">
 
             <span class="category">${news.category}</span>
 
             <h1>${news.title}</h1>
 
-            <p><strong>${news.date}</strong></p>
+            <p>${news.date}</p>
 
             <img src="${news.image}" class="single-image">
 
-            <p class="summary">
+            <p class="summary">${news.summary}</p>
 
-            ${news.summary}
-
-            </p>
-
-            <div class="content">
-
-            ${news.content}
-
-            </div>
+            <div class="content">${news.content}</div>
 
             <br>
 
-            <a href="index.html" class="read-btn">
-
-            ← Home
-
-            </a>
+            <a href="index.html" class="read-btn">← Home</a>
 
         </div>
-
         `;
 
-    }
-
-    catch(error){
+    } catch (error) {
 
         console.error(error);
 
-        container.innerHTML = "<h2>Error Loading News</h2>";
+        container.innerHTML = `
+            <h2>Error Loading News</h2>
+            <pre>${error.message}</pre>
+        `;
 
     }
 
