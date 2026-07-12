7591f0c0f689d12407569b280074734d829aa6d6
the khabar thread <thekhabarthread@gmail.com>
2026-07-12 12:39:32 +0530

    Update news.js

---
+++ data/news.js
@@ -0,0 +1,88 @@
+import { db } from "./js/firebase.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get("id");
+
+const container = document.getElementById("news-details");
+
+async function loadNews() {
+
+    if (!id) {
+
+        container.innerHTML = "<h2>News Not Found</h2>";
+
+        return;
+
+    }
+
+    try {
+
+        const docRef = doc(db, "news", id);
+
+        const docSnap = await getDoc(docRef);
+
+        if (!docSnap.exists()) {
+
+            container.innerHTML = "<h2>News Not Found</h2>";
+
+            return;
+
+        }
+
+        const news = docSnap.data();
+
+        container.innerHTML = `
+
+        <div class="single-news">
+
+            <span class="category">${news.category}</span>
+
+            <h1>${news.title}</h1>
+
+            <p><strong>${news.date}</strong></p>
+
+            <img src="${news.image}" class="single-image">
+
+            <p class="summary">
+
+            ${news.summary}
+
+            </p>
+
+            <div class="content">
+
+            ${news.content}
+
+            </div>
+
+            <br>
+
+            <a href="index.html" class="read-btn">
+
+            ← Home
+
+            </a>
+
+        </div>
+
+        `;
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        container.innerHTML = "<h2>Error Loading News</h2>";
+
+    }
+
+}
+
+loadNews();
