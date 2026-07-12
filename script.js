410f6be6f8d590fc324530a4bb8a9fc2f2dee276
the khabar thread <thekhabarthread@gmail.com>
2026-07-12 12:10:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,91 @@
 import { db } from "./js/firebase.js";
 
 import {
-    collection,
-    getDocs,
-    query,
-    orderBy
+  collection,
+  getDocs,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+async function loadNews() {
+
+  try {
+
+    const newsRef = collection(db, "news");
+
+    const q = query(newsRef, orderBy("createdAt", "desc"));
+
+    const snapshot = await getDocs(q);
+
+    const news = [];
+
+    snapshot.forEach((doc) => {
+
+      news.push(doc.data());
+
+    });
+
+    if (news.length === 0) {
+
+      document.getElementById("news-grid").innerHTML =
+        "<h2>No News Found</h2>";
+
+      return;
+
+    }
+
+    // HERO
+
+    document.getElementById("hero-category").innerText =
+      news[0].category;
+
+    document.getElementById("hero-title").innerText =
+      news[0].title;
+
+    document.getElementById("hero-summary").innerText =
+      news[0].summary;
+
+    document.getElementById("hero-image").src =
+      news[0].image;
+
+    // GRID
+
+    const grid = document.getElementById("news-grid");
+
+    grid.innerHTML = "";
+
+    news.forEach((item) => {
+
+      grid.innerHTML += `
+
+      <div class="card">
+
+        <img src="${item.image}" class="card-image">
+
+        <h3>${item.title}</h3>
+
+        <p>${item.summary}</p>
+
+        <a href="#" class="read-btn">
+
+        पूरा पढ़ें →
+
+        </a>
+
+      </div>
+
+      `;
+
+    });
+
+  }
+
+  catch (error) {
+
+    console.error(error);
+
+  }
+
 }
-from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-const newsRef = collection(db, "news");
-
-const q = query(newsRef, orderBy("createdAt", "desc"));
-
-const snapshot = await getDocs(q);
-
-const news = [];
-
-snapshot.forEach((doc) => {
-
-    news.push(doc.data());
-
-});
-
-// HERO
-
-document.getElementById("hero-category").innerText = news[0].category;
-
-document.getElementById("hero-title").innerText = news[0].title;
-
-document.getElementById("hero-summary").innerText = news[0].summary;
-
-document.getElementById("hero-image").src = news[0].image;
-
-
-// NEWS GRID
-
-const grid = document.getElementById("news-grid");
-
-grid.innerHTML = "";
-
-news.forEach(item => {
-
-grid.innerHTML += `
-
-<div class="card">
-
-<img src="${item.image}" class="card-image">
-
-<h3>${item.title}</h3>
-
-<p>${item.summary}</p>
-
-<a href="#" class="read-btn">
-
-पूरा पढ़ें →
-
-</a>
-
-</div>
-
-`;
-
-});
+loadNews();
