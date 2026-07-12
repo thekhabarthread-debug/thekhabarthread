f43a05e5f703803ec1a532e367f107509f98f416
the khabar thread <thekhabarthread@gmail.com>
2026-07-12 11:26:57 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDX-1DUOIoKEUUw_Nwj_A4N54Q48Aec2EQ",
-  authDomain: "the-khabar-thread.firebaseapp.com",
-  projectId: "the-khabar-thread",
-  storageBucket: "the-khabar-thread.firebasestorage.app",
-  messagingSenderId: "1035108939429",
-  appId: "1:1035108939429:web:e1192a467fd69e58f2ff4c"
+
+apiKey: "AIzaSyDX-1DUOIoKEUUw_Nwj_A4N54Q48Aec2EQ",
+
+authDomain: "the-khabar-thread.firebaseapp.com",
+
+projectId: "the-khabar-thread",
+
+storageBucket: "the-khabar-thread.firebasestorage.app",
+
+messagingSenderId: "1035108939429",
+
+appId: "1:1035108939429:web:e1192a467fd69e58f2ff4c"
+
 };
 
 export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
