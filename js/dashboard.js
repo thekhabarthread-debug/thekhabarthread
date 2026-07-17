1aab772450c24bc6c7af2edd4bb383c7f1965ce0
the khabar thread <thekhabarthread@gmail.com>
2026-07-17 05:59:54 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,18 @@
 import { auth, logout } from "./auth.js";
+import { db } from "./firebase.js";
 
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-const ADMIN_EMAIL = "thekhabarthread@gmail.com";   // अपना Admin Gmail
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const ADMIN_EMAIL = "thekhabarthread@gmail.com";
 
 const logoutBtn = document.getElementById("logoutBtn");
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
 
     if (!user) {
         window.location.href = "login.html";
@@ -21,7 +27,52 @@ onAuthStateChanged(auth, (user) => {
 
     console.log("Admin Login Successful");
 
+    loadStats();
+
 });
+
+async function loadStats(){
+
+const snapshot = await getDocs(collection(db,"news"));
+
+let total = 0;
+let featured = 0;
+let breaking = 0;
+
+const categories = new Set();
+
+snapshot.forEach((doc)=>{
+
+const news = doc.data();
+
+total++;
+
+if(news.featured === true){
+
+featured++;
+
+}
+
+if(news.breaking === true){
+
+breaking++;
+
+}
+
+if(news.category){
+
+categories.add(news.category);
+
+}
+
+});
+
+document.getElementById("totalNews").innerText = total;
+document.getElementById("featuredNews").innerText = featured;
+document.getElementById("breakingNews").innerText = breaking;
+document.getElementById("totalCategory").innerText = categories.size;
+
+}
 
 logoutBtn.addEventListener("click", async () => {
 
