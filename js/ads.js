910c14908bd775a3ccba4ef5ad60869adeace143
the khabar thread <thekhabarthread@gmail.com>
2026-07-19 16:52:06 +0530

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,4 +1,5 @@
 import { db } from "./firebase.js";
+import { auth } from "./auth.js";
 
 import {
 collection,
@@ -8,6 +9,22 @@ orderBy,
 deleteDoc,
 doc
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const ADMIN_EMAIL = "thekhabarthread@gmail.com";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    alert("Aap login nahi hain. Login page par bhej rahe hain.");
+    window.location.href = "login.html";
+    return;
+  }
+  if (user.email !== ADMIN_EMAIL) {
+    alert("Access Denied");
+    window.location.href = "login.html";
+  }
+});
 
 /*=========================================
 LOAD ADS
